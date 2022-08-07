2aeae46172342758366242fd7380cad785b7b043
Xpressglobe <tobeodiachi@yahoo.com>
2022-08-07 21:51:11 +0100

    complete

---
+++ resources/views/front/terms-and-conditions.blade.php
@@ -0,0 +1,85 @@
+@include('front.template.header2')
+  
+<!-- section content begin -->
+<div class="uk-section">
+    <div class="uk-container">
+        <div class="uk-grid uk-flex uk-flex-center">
+            <div class="uk-width-3-5@m">
+                <div class="uk-grid-small uk-child-width-1-2@s uk-child-width-1-1@m in-content-11" data-uk-grid>
+                    <div>
+                        <a href="#" class="uk-card uk-card-small uk-card-default uk-card-body uk-border-rounded uk-display-block">
+                            <div class="uk-grid" data-uk-grid>
+                                <div class="uk-width-auto@m uk-flex uk-flex-middle">
+                                    <i class="fas fa-book fa-lg in-icon-wrap in-margin-remove-left@s uk-margin-left"></i>
+                                </div>
+                                <div class="uk-width-expand@m in-margin-small-top@s">
+                                    <h4 class="uk-margin-small-bottom">Getting started with our product</h4>
+                                    
+                                    <p class="uk-text-small uk-margin-remove-bottom">
+                                        When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service.<br>
+                                        You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service. You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account.
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#" class="uk-card uk-card-small uk-card-default uk-card-body uk-border-rounded uk-display-block">
+                            <div class="uk-grid" data-uk-grid>
+                                <div class="uk-width-auto@m uk-flex uk-flex-middle">
+                                    <i class="fas fa-book fa-lg in-icon-wrap in-margin-remove-left@s uk-margin-left"></i>
+                                </div>
+                                <div class="uk-width-expand@m in-margin-small-top@s">
+                                    <h4 class="uk-margin-small-bottom">Links To Other Web Sites</h4>
+                                    
+                                    <p class="uk-text-small uk-margin-remove-bottom">
+                                        
+                                        Our Service may contain links to third-party web sites or services that are not owned or controlled by Profit Trending. Profit Trending has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Profit Trending shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.<br>
+                                        
+                                        We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.
+                                        
+                                        
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+
+
+                    
+
+<div>
+    <a href="#" class="uk-card uk-card-small uk-card-default uk-card-body uk-border-rounded uk-display-block">
+        <div class="uk-grid" data-uk-grid>
+            <div class="uk-width-auto@m uk-flex uk-flex-middle">
+                <i class="fas fa-book fa-lg in-icon-wrap in-margin-remove-left@s uk-margin-left"></i>
+            </div>
+            <div class="uk-width-expand@m in-margin-small-top@s">
+                <h4 class="uk-margin-small-bottom">Termination</h4>
+                
+                <p class="uk-text-small uk-margin-remove-bottom">
+                    
+                    We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.
+
+                    Upon termination, your right to use the Service will immediately cease. If you wish to terminate your account, you may simply discontinue using the Service.
+                    
+                    
+                </p>
+                
+            </div>
+        </div>
+    </a>
+</div>
+
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- section content end -->
+</main>
+@include('front.template.footer')
