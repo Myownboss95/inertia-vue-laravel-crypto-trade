4a6e06b9009875859f3686b051456b685407ac68
Perekpo Paulinus Emmanuel <perepaul15@gmail.com>
2022-08-11 19:06:39 +0100

    new build files

---
+++ public/build/assets/error.e7db4921.js
@@ -0,0 +1,1 @@
+import{q as a,v as s,a as o,b as t,t as n}from"./main.a4388b13.js";const i={name:"error",props:["name"],setup(e){return(r,p)=>a((o(),t("span",{class:"font-size-12 text-danger"},n(r.$page.props.errors[e.name]),513)),[[s,r.$page.props.errors[e.name]]])}};export{i as _};
