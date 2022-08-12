71b6f988942ec178c47b1a25844e00cbdcee6c96
Perekpo Paulinus Emmanuel <perepaul15@gmail.com>
2022-08-12 22:39:37 +0100

    addapt to trading chart

--- resources/views/pages/admin/trades/trades.vue
+++ resources/views/pages/admin/trades/trades.vue
@@ -1,11 +1,11 @@
 <template>
   <Head title="Trades" />
   <breadcrumb title="Trades" :crumbs="['Dashboard', 'Trades']" />
-  <div class="row p-3 m-1">
-    <div class="card shadow col-md-4 col-sm-12 ">
-    <div class="card-body">
-      <!-- Trading view widget -->
-      <form>
+  <div class="row mt-3">
+    <div class="card shadow col-md-4 col-sm-12">
+      <div class="card-body">
+        <!-- Trading view widget -->
+        <form>
           <FormGroup
             name="amount"
             placeholder="Enter Amount"
@@ -13,15 +13,21 @@
             v-model="form.amount"
           />
 
-        <FormSelect
+          <FormSelect
             id="tradeable_type"
             name="tradeable_type"
             label="Asset type"
-            :options="{ '':'Select Asset type', 'crypto': 'Crypto Currency', 'commodity': 'Commodity', 'currency':'Currency' }"
+            :options="{
+              '': 'Select Asset type',
+              crypto: 'Crypto Currency',
+              commodity: 'Commodity',
+              currency: 'Currency',
+            }"
             v-model="form.tradeable_type"
           />
 
           <FormSelect
+            class="mt-3"
             id="asset"
             name="tradeable_id"
             label="Asset"
@@ -30,6 +36,7 @@
           />
 
           <FormSelect
+            class="mt-3"
             id="stop_loss"
             name="stop_loss"
             label="Stop Loss"
@@ -37,85 +44,92 @@
             v-model="form.stop_loss"
           />
 
-
-          <div class="d-flex justify-content-between">
-          <FormButton
-            type="button"
-            class="w-100 btn btn-success mt-3 m-1"
-            :disabled="form.processing"
-            @button-clicked="placeTrade('buy')"
-          >
-            <ButtonLoader text="Buy" :loading="form.processing" />
-          </FormButton>
-          <FormButton
-            type="button"
-            class="w-100 btn btn-danger mt-3 m-1"
-            :disabled="form.processing"
-            @button-clicked="placeTrade('sell')"
-          >
-            <ButtonLoader text="Sell" :loading="form.processing" />
-          </FormButton>
+          <div class="d-flex justify-content-between mt-2">
+            <FormButton
+              type="button"
+              class="w-100 btn btn-success mt-3 m-1"
+              :disabled="form.processing"
+              @button-clicked="placeTrade('buy')"
+            >
+              <ButtonLoader text="Buy" :loading="form.processing" />
+            </FormButton>
+            <FormButton
+              type="button"
+              class="w-100 btn btn-danger mt-3 m-1"
+              :disabled="form.processing"
+              @button-clicked="placeTrade('sell')"
+            >
+              <ButtonLoader text="Sell" :loading="form.processing" />
+            </FormButton>
           </div>
         </form>
-
+      </div>
     </div>
-  </div>
-  <div class="card shadow col-md-8 col-sm-12">
-    <div class="card-body">
-      <!-- Trading view widget -->
-      <Chart :options="{theme: 'light',autosize: true,}" style="height:400px; width:100%"/>
+    <div class="card shadow col-md-8 col-sm-12">
+      <div class="card-body">
+        <!-- Trading view widget -->
+        <Chart
+          :options="{ theme: theme.theme ?? 'light', autosize: true }"
+          style="height: 400px; width: 100%"
+        />
+      </div>
     </div>
-  </div>
   </div>
 </template>
 
 <script setup>
-import { computed, ref, watch } from "vue";
-import { useForm } from '@inertiajs/inertia-vue3';
-import FormGroup from '@/views/components/form/FormGroup.vue';
-import FormSelect from '@/views/components/form/FormSelect.vue';
-import FormButton from '@/views/components/form/FormButton.vue';
+  import { computed, ref, watch } from 'vue';
+  import { useForm } from '@inertiajs/inertia-vue3';
+  import FormGroup from '@/views/components/form/FormGroup.vue';
+  import FormSelect from '@/views/components/form/FormSelect.vue';
+  import FormButton from '@/views/components/form/FormButton.vue';
 import ButtonLoader from '@/views/components/form/ButtonLoader.vue';
-import { error } from "@/js/toast";
-import axios from "axios";
+  import breadcrumb from '@/views/components/layout/breadcrumb.vue';
+  import { error } from '@/js/toast';
+  import axios from 'axios';
 
-import { Chart } from 'vue-tradingview-widgets'
+  import { useTheme } from '@/stores/theme';
 
-const assets = ref(null);
+  import { Chart } from 'vue-tradingview-widgets';
 
-const props = defineProps({
-  trades: Object,
-});
-const trades = computed(() => props.trades.data);
-const links = computed(() => props.trades.links);
+  const theme = useTheme();
 
-const form = useForm({
+  const assets = ref(null);
+
+  const props = defineProps({
+    trades: Object,
+  });
+  const trades = computed(() => props.trades.data);
+  const links = computed(() => props.trades.links);
+
+  const form = useForm({
     name: '',
     type: '',
     tradeable_type: '',
-    stop_loss: ''
+    stop_loss: '',
   });
 
-  watch(()=>form.tradeable_type, (type) => {
-    if(type != ''){
-        axios.get(route('user.trades.getTradeables',{type}))
-        .then(res => {
-            if(res.status != 200) throw Error()
+  watch(
+    () => form.tradeable_type,
+    (type) => {
+      if (type != '') {
+        axios
+          .get(route('user.trades.getTradeables', { type }))
+          .then((res) => {
+            if (res.status != 200) throw Error();
             assets.value = res.data.data;
-
-        })
-        .catch(err => {
+          })
+          .catch((err) => {
             error('Failed to load assets, refresh page.');
-        })
+          });
+      }
     }
-  })
+  );
 
-    const placeTrade = (type) => {
-        form.type = type;
-        form.post(route('admin.tradeables.store'));
+  const placeTrade = (type) => {
+    form.type = type;
+    form.post(route('admin.tradeables.store'));
   };
-
-
 </script>
 
 <style>
