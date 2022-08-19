f7c12c839ec9540e90064cd6f70412b2a51f5d29
Perekpo Paulinus Emmanuel <perepaul15@gmail.com>
2022-08-19 11:29:31 +0100

    height and width:

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-    <img src="{{asset('logo_algo.png')}}" class="logo" alt="Laravel Logo">
+    <img src="{{asset('logo_algo.png')}}" class="logo" alt="Laravel Logo" style="height: 120px; width:150px">
 {{-- @if (trim($slot) === 'Laravel')
 @else
 {{ $slot }}
