60bcd2401053ecce00a774ad29fb7cdd9d9316e1
Perekpo Paulinus Emmanuel <perepaul15@gmail.com>
2022-09-15 14:25:29 +0100

    is_admin check experiment

--- resources/views/components/layout/sidebar.vue
+++ resources/views/components/layout/sidebar.vue
@@ -130,7 +130,8 @@
   import SidebarItem from './sidebarItem.vue';
   import { usePage } from '@inertiajs/inertia-vue3';
 
-const is_admin = computed(() => usePage().props.value.auth.user.is_admin);
+const is_admin = usePage().props.value.auth.user.is_admin == 0;
+
 console.log(is_admin);
 
 const botsMenu = computed(() => is_admin == true ? 'Trade Bots' : 'Trade Bot');
