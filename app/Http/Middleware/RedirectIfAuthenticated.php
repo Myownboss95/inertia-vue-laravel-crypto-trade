7aeaa770eee6a8692cfaee9481b82f3e168b57df
Perekpo Paulinus Emmanuel <perepaul15@gmail.com>
2022-09-15 13:39:32 +0100

    redirect if authenticated

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,11 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect()->intended($request->user()->is_admin ? route('admin.index') : route('user.index'));
+                return redirect()->route(
+                    $request->user()->is_admin
+                        ? 'admin.index'
+                        : 'user.index'
+                );
             }
         }
 
