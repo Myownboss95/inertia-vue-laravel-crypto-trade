33f70e4bd92acc182f140fb68c04efb25f348563
Perekpo Paulinus Emmanuel <perepaul15@gmail.com>
2022-09-25 18:37:51 +0100

    logo alt text changed

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-    <img src="{{asset(config('app.logo'))}}" class="logo" alt="Laravel Logo" style="height: 120px; width:150px">
+    <img src="{{asset(config('app.logo'))}}" class="logo" alt="{{config('app.name')}} Logo" style="height: 120px; width:150px">
 {{-- @if (trim($slot) === 'Laravel')
 @else
 {{ $slot }}
