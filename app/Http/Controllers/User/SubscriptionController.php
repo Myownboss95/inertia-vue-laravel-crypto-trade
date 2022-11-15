034600f4c5b33c25e33b8803fd441064c1f8d821
= <perepaul15@gmail.com>
2022-11-15 22:39:45 +0100

    no deduction

--- app/Http/Controllers/User/SubscriptionController.php
+++ app/Http/Controllers/User/SubscriptionController.php
@@ -42,11 +42,9 @@ class SubscriptionController extends Controller
             'expires_at' => Carbon::now()->addDays("$plan->tenure")->toDateTimeString(),
         ]);
 
-        $account->save();
-        $investedAccount->save();
+        // $account->save();
+        // $investedAccount->save();
         session()->flash('success', 'Subscribed successfully');
         return redirect()->route('user.index');
-
-
     }
 }
