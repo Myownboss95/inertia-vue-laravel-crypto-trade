9b579923f491134a74fc2e76f9e1172a69ae091b
= <perepaul15@gmail.com>
2023-01-07 21:46:36 +0100

    maintenance mode message

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Maintenance'))
+@section('code', '503')
+@section('message', __("Sorry, we're down for scheduled maintenance right now. Check back later."))
